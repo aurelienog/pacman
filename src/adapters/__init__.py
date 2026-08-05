8e0a90edbff0d8e7108d72b248e8de4d46e1ea5b
Aurelie Nogueira <aurelienog@gmail.com>
2026-08-05 17:47:42 +0200

    update: expose adapters

---
+++ src/adapters/__init__.py
@@ -0,0 +1,5 @@
+from .config_loader import ConfigLoader
+from .amazing_maze_factory import AmazingMazeFactory
+from .json_highscore_repository import JsonHighscoreRepository
+
+__all__ = ["ConfigLoader", "AmazingMazeFactory", "JsonHighscoreRepository"]
