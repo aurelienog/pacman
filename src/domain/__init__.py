09a790baff6f06a83d9897b944d7996eccb1e748
Oleksii Shtohrin <oshtohri@c3r6s4.42urduliz.com>
2026-08-06 11:26:30 +0200

    Update imports in src/domain add classes Defaults and Limits

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,11 +1,21 @@
-from .config import GameConfig, LevelConfig
+from .config import Defaults, Limits, GameConfig, LevelConfig
 from .maze import Maze
 from .geometry import Direction, Position
 from .player import Player
 from .ghost import Ghost, GhostMode
 from .item import Item, ItemKind
 
-
-__all__ = ["GameConfig", "LevelConfig", "Maze",
-           "Direction", "Position", "Player", "Ghost", "GhostMode",
-           "Item", "ItemKind"]
+__all__ = [
+    "Defaults",
+    "Limits",
+    "GameConfig",
+    "LevelConfig",
+    "Maze",
+    "Direction",
+    "Position",
+    "Player",
+    "Ghost",
+    "GhostMode",
+    "Item",
+    "ItemKind"
+]
